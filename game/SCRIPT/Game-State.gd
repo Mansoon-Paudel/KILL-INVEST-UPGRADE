3b783bf95a04c91a8d3c65c2130195a875ddd180
Mansoon Paudel <mansoonpaudel@gmail.com>
2026-04-14 16:26:39 +0545

    Successful set up of Global Camera 2D bound according to tile map

--- game/SCRIPT/Game-State.gd
+++ game/SCRIPT/Game-State.gd
@@ -1,10 +1,17 @@
-extends Node
+class_name manager extends Node
 
 var level = 1
 var Coin = 0
 var Crystal = 0
-
-
 var enemy_kill_count = 0
 var current_zone = "zone_1"
 var player_position = Vector2.ZERO
+
+var current_tilemap_bounds : Array[Vector2]
+signal TileMapBoundsChanged(bounds:Array[Vector2])
+
+func ChangeTilemapBounds(bounds:Array[Vector2]) -> void:
+	current_tilemap_bounds=bounds
+	TileMapBoundsChanged.emit(bounds)
+	pass
+	
