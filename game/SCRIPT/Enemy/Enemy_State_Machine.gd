6efa5ef286c4d38751aa57d2b1cc5e451c3ef7ca
Mansoon Paudel <mansoonpaudel@gmail.com>
2026-04-15 10:48:01 +0545

    added killable enemy

--- game/SCRIPT/Enemy/Enemy_State_Machine.gd
+++ game/SCRIPT/Enemy/Enemy_State_Machine.gd
@@ -1,42 +1,36 @@
 class_name EnemyStateMachine extends Node
 
-
 var states: Array[EnemyState]
-var prev_state : EnemyState
-var current_state : EnemyState
-
+var prev_state: EnemyState
+var current_state: EnemyState
 
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_DISABLED
-	
+
 func _process(delta: float) -> void:
 	change_state(current_state.process(delta))
-	pass
-func _physics_process(delta: float) -> void:
-	change_state(current_state.process(delta))
 
-func initialize (_enemy: Enemy) -> void:
+func _physics_process(delta: float) -> void:
+	change_state(current_state.physics(delta))  # was calling process() here by mistake
+
+func initialize(_enemy: Enemy) -> void:
 	states = []
-	
 	for c in get_children():
 		if c is EnemyState:
 			states.append(c)
-			
-	for s in states :
-		s.enemy=_enemy
-		s.state_machine=self
+	for s in states:
+		s.enemy = _enemy
+		s.state_machine = self
 		s._init()
-	
 	if states.size() > 0:
 		change_state(states[0])
-		process_mode=Node.PROCESS_MODE_INHERIT
+		process_mode = Node.PROCESS_MODE_INHERIT
 
-func change_state(new_state:EnemyState):
+func change_state(new_state: EnemyState) -> void:
 	if new_state == null || new_state == current_state:
-			return
-	
+		return
 	if current_state:
 		current_state.exit()
-	prev_state =current_state
-	current_state=new_state
+	prev_state = current_state
+	current_state = new_state
 	current_state.enter()
--- game/SCRIPT/Enemy/enemy.gd
+++ game/SCRIPT/Enemy/enemy.gd
@@ -1,53 +1,31 @@
 class_name Enemy extends CharacterBody2D
 
-
-signal direction_changed(new_direction:Vector2)
+signal direction_changed(new_direction: Vector2)
 signal enemy_damaged()
-const DIR4 = [Vector2.RIGHT, Vector2.DOWN, Vector2.UP, Vector2.LEFT]
-@export var hp : int= 3 
-@onready var sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+const DIR4 = [Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT, Vector2.UP]
+
+@export var hp: int = 3
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var hitbox: HitBox = $hitbox
 @onready var state_machine: EnemyStateMachine = $EnemyStateMachine
 
 var cardinal_direction: Vector2 = Vector2.DOWN
-var direction: Vector2 =Vector2.ZERO
-var player : Player
 var invulnerable: bool = false
 
-
 func _ready() -> void:
+	hitbox.Damaged.connect(_on_damaged)
 	state_machine.initialize(self)
-	player=GameState.player
 
-func _physics_process(delta: float) -> void:
-	move_and_slide()
+func set_direction(dir: Vector2) -> void:
+	if dir == Vector2.ZERO:
+		return
+	cardinal_direction = dir
+	direction_changed.emit(cardinal_direction)
 
-
-func set_direction(_new_direction : Vector2) -> bool:
-	direction = _new_direction
-	if direction == Vector2.ZERO:
-		return false
-
-	var direction_id:int = int(round(
-		(direction+cardinal_direction*0.1).angle()/TAU * DIR4.size()
-	))
-	
-	var new_dir = DIR4[direction_id]
-	
-	if new_dir == cardinal_direction:
-		return false
-	direction_changed.emit(new_dir)
-	 
-	if cardinal_direction == Vector2.LEFT:
-		sprite_2d.scale.x=-1
-	else:
-		sprite_2d.scale.x=1
-	
-	return true
-
-func UpdateAnimation(state: String) -> void:
-	sprite_2d.play(state + "_" + AnimDirection())
-
+func UpdateAnimation(anim_name: String) -> void:
+	animated_sprite_2d.play(anim_name + "_" + AnimDirection())
 
 func AnimDirection() -> String:
 	if cardinal_direction == Vector2.UP:
@@ -58,3 +36,25 @@ func AnimDirection() -> String:
 		return "Left"
 	else:
 		return "Right"
+
+func _on_damaged(damage: int) -> void:
+	if invulnerable:
+		return
+	hp -= damage
+	emit_signal("enemy_damaged")
+	print("Enemy HP: ", hp)
+	if hp <= 0:
+		die()
+
+func die() -> void:
+	queue_free()
+func _physics_process(delta: float) -> void:
+	move_and_slide()
+	for i in get_slide_collision_count():
+		var col = get_slide_collision(i)
+		if col.get_collider() is Player:
+			
+			var push_dir = col.get_collider().global_position - global_position
+			col.get_collider().velocity += push_dir.normalized() * 200
+			
+			global_position -= col.get_normal() * col.get_depth()
